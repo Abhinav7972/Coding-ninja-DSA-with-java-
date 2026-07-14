9ff9a7339a8cfe55a966d78c3093f8aca77e2bf4
abhinav soni <abhinavsoni316@gmail.com>
2026-07-14 18:13:42 +0530

    Refactor Individual_digit.java to reverse and print individual digits of a number

--- introduction_to_java/Loops/Individual_digit.java
+++ introduction_to_java/Loops/Individual_digit.java
@@ -4,10 +4,14 @@ public class Individual_digit {
 public static void main(String[] args) {
 Scanner s = new Scanner(System.in);
 int n = s.nextInt();
+int rn = 0;
 while (n!=0) {
-    System.out.println(n%10);
+    int ln = n%10;
+    rn = rn * 10 + ln;
     n=n/10;
 }
+
+System.out.println(rn);
 
 s.close();
 }
