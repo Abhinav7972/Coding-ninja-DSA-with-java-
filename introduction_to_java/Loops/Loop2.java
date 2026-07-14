55127e62ca8d030feaa7e6d102a1be70199718ea
abhinav soni <abhinavsoni316@gmail.com>
2026-07-14 17:34:56 +0530

    Add Loop2.java to demonstrate a while loop that prints numbers from 1 to 10

---
+++ introduction_to_java/Loops/Loop2.java
@@ -0,0 +1,16 @@
+public class Loop2 {
+public static void main(String[] args) {
+ //while loop 
+
+//check condition
+//if true body of loop 
+//inside body of loop loop operation of updating values like increament
+//if false exit body of loop
+
+ int i = 1;
+ while (i <=10) {
+  System.out.print(i + " ");
+  i++;
+ }
+}
+}
