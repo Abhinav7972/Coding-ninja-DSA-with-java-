d8e886e931ac4bb6c5edc365d950830a11ed708c
abhinav soni <abhinavsoni316@gmail.com>
2026-07-19 11:52:05 +0530

    implement hourglass pattern

---
+++ introduction_to_java/Patterns_2/HourGlass_pattern.java
@@ -0,0 +1,38 @@
+public class HourGlass_pattern {
+public static void main(String[] args) {
+    int n=5;
+   for(int i=1;i<=n;i++)
+   {
+    for(int s=1;s<=i-1;s++)
+    {
+        System.out.print(" ");
+    }
+
+    for(int j=i;j<=n;j++)
+    {
+        System.out.print(j + " ");
+    }
+
+    System.out.println();
+    
+   }
+
+
+
+    for(int i=n-1;i>=1;i--)
+   {
+    for(int s=1;s<=i-1;s++)
+    {
+        System.out.print(" ");
+    }
+
+    for(int j=i;j<=n;j++)
+    {
+        System.out.print(j + " ");
+    }
+
+    System.out.println();
+    
+   }
+}
+}
