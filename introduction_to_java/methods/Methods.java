6c058d168c8223d8094b9910af90713dcb532078
abhinav soni <abhinavsoni316@gmail.com>
2026-07-20 10:58:04 +0530

    impliment method without arg

---
+++ introduction_to_java/methods/Methods.java
@@ -0,0 +1,16 @@
+
+
+public class Methods {
+
+public static void printDetails()
+{
+    System.out.println("Name : alpha man");
+    System.out.println("DOB: 12/09/1994");
+    System.out.println("Account number : 00012345678");
+}
+
+
+public static void main(String[] args) {
+ printDetails();
+}
+}
