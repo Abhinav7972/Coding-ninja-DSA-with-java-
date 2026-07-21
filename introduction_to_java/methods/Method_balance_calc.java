a839c613987b22b50fb8b20c4c8a934fb7778b70
abhinav soni <abhinavsoni316@gmail.com>
2026-07-21 11:16:49 +0530

    printed another calculate balance method with diff value

--- introduction_to_java/methods/Method_balance_calc.java
+++ introduction_to_java/methods/Method_balance_calc.java
@@ -1,6 +1,9 @@
 public class Method_balance_calc {
  public static void main(String[] args) {
   printDetails("Abhinav", "AC1234", 100000, 10000, 0);
+  System.out.println();
+  printDetails("A2", "AB2346", 200000, 40000, 10000);
+
  }
 
  public static void printDetails(String name,String acc,double cur_bal,double debit, double credit)
