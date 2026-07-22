b6d19695ffde9e643dc957ed42d1dcc02a9ed13a
abhinav soni <abhinavsoni316@gmail.com>
2026-07-22 12:03:19 +0530

    impliment array declaration

---
+++ introduction_to_java/Arrays_1/Array_basic.java
@@ -0,0 +1,15 @@
+//package introduction_to_java.Arrays_1;
+
+public class Array_basic {
+public static void main(String[] args) {
+    //declaring var 
+    int a =10;
+
+
+    //declaring array 
+    int arr[] = new int[10];
+    int []arr1;
+
+
+}
+}
