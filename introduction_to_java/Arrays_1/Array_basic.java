fd569ca9ce836aaf23b2a1e170300879b9789066
abhinav soni <abhinavsoni316@gmail.com>
2026-07-22 12:13:56 +0530

    impliment array value assigning and retriving

--- introduction_to_java/Arrays_1/Array_basic.java
+++ introduction_to_java/Arrays_1/Array_basic.java
@@ -8,7 +8,17 @@ public static void main(String[] args) {
 
     //declaring array 
     int arr[] = new int[10];
-    int []arr1;
+    //int []arr1;
+
+   //assighning value to array
+   arr[0] = 2;
+   arr[1] =4;
+   arr[2] = 5;
+   arr[9] =10;
+
+ //retriving data from array 
+  System.out.println(arr[1]); //access element by index number
+  System.out.println(arr[9]);
 
 
 }
