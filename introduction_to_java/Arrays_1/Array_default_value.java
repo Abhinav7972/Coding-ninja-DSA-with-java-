0294391003f3ab941cc32fef4f743ba7e431f1a0
abhinav soni <abhinavsoni316@gmail.com>
2026-07-22 22:05:03 +0530

    impliment program to return default value of diff array types

---
+++ introduction_to_java/Arrays_1/Array_default_value.java
@@ -0,0 +1,21 @@
+public class Array_default_value {
+public static void main(String[] args) {
+    int a[] = new int[5];
+    byte b[] = new byte[5];
+    short s[] = new short[5];
+    float f[] = new float[5];
+    double d[] = new double[5];
+    char c[] = new char[5];
+    String str[] = new String[5];
+    boolean bool[] = new boolean[5];
+
+    System.out.println(a[0]);
+    System.out.println(b[0]);
+    System.out.println(s[0]);
+    System.out.println(f[0]);
+    System.out.println(d[0]);
+    System.out.println(c[0]);
+    System.out.println(str[0]);
+    System.out.println(bool[0]);
+}
+}
