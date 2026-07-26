2543a2683474460bd102e76a25fe4a88ebed35e5
abhinav soni <abhinavsoni316@gmail.com>
2026-07-26 19:35:52 +0530

    impliment code to understand char array fundamental

---
+++ introduction_to_java/strings/Char_array.java
@@ -0,0 +1,18 @@
+
+
+public class Char_array {
+  public static void main(String[] args) {
+    char ch [] = {'a','b','c','d'};
+    int i [] = {1,2,3,4}; 
+     
+    System.out.println(i); //integer array return  memory address
+    System.out.println(ch); //character array return array value
+    System.out.println(Character.toUpperCase(ch[0])); // string method on array
+    System.out.println(ch.toString()); // to get memory address of char array we  convert it to string 
+    
+
+
+
+    
+  }
+}
