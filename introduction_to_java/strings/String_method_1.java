b61fed0c8439db2ebcee6a3293799769a85f7254
abhinav soni <abhinavsoni316@gmail.com>
2026-07-26 20:15:36 +0530

    impliment program of basic string method

---
+++ introduction_to_java/strings/String_method_1.java
@@ -0,0 +1,33 @@
+public class String_method_1 {
+ public static void main(String[] args) {
+    String str = "   Alpha man is a goal    ";
+
+    //string method 
+
+    //length 
+    System.out.println(str.length());  // finding length of string
+
+    //charAt 
+    System.out.println(str.charAt(2)); //finding string char at specific pos 
+
+    //tolowerCase 
+    System.out.println(str.toLowerCase()); // convert to lower case 
+
+    //toUppercase 
+    System.out.println(str.toUpperCase()); //convert to upper case 
+
+    //trim 
+    System.out.println(str.trim()); //remove trailing and leading space
+
+    //substring 
+    System.out.println(str.substring(2,10)); // use to extract particular part of string 
+    System.out.println(str.substring(9)); //extract substring from start index   
+
+    //replace 
+    System.out.println(str.replace('a','k')); //use to replace char in string 
+
+   //indexof 
+   System.out.println(str.indexOf('p')); //use to find index of char in string
+
+ }
+}
