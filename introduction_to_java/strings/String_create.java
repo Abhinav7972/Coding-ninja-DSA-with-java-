ef2ca6cef24775734789f642dc8fc240b2c0efb4
abhinav soni <abhinavsoni316@gmail.com>
2026-07-26 19:50:55 +0530

    impliment program to create string and some string method

---
+++ introduction_to_java/strings/String_create.java
@@ -0,0 +1,13 @@
+public class String_create {
+ public static void main(String[] args) {
+   String s1 = "Alpha";
+   String s2 = "Man"; 
+
+   String s3 = s1.concat(s2); //concat method of string 
+
+   System.out.println(s3); //printing concated strings
+   System.out.println(s1.toUpperCase()); //converting string to upper case 
+   System.out.println(s1.charAt(2)); //return character at particular place 
+   
+ }
+}
