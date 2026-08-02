2a90f8518d74582e847448fe083358181c0cc13b
abhinav soni <abhinavsoni316@gmail.com>
2026-08-02 16:04:27 +0530

    impliment program for binary search in array

---
+++ java_basics/searching_algorithm/Binary_search.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Binary_search {
+ public static int search(int arr[],int key)
+ {
+int left =0,right=arr.length-1;
+
+while(left<=right)
+{
+int mid = left+right/2; 
+
+    if(arr[mid]==key)
+    {
+       return mid;
+    }
+    else if(arr[mid]<key)
+    {
+      left=mid+1;
+    }
+    else 
+    {
+        right =mid-1;
+    }
+}
+
+ return -1;
+ }
+
+
+ public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+  int arr[] = {1,2,3,4,5,6,7,8}; 
+  int n = sc.nextInt(); 
+  int result = search(arr, n);
+  System.out.println(result);
+  sc.close();
+ }
+}
