9af348f31459fd133aa14684e99bdbbd2be5e6e3
abhinav soni <abhinavsoni316@gmail.com>
2026-08-05 22:26:49 +0530

    impliment fibonacci method using recursion

--- java_basics/recursion/recursion_1java.java
+++ java_basics/recursion/recursion_1java.java
@@ -39,10 +39,29 @@ printnum(n-1);
 System.out.print(n+" ");
 }
 
+
+public static int fibnum(int n)
+{
+if(n==1 || n==2)
+{
+ return 1;
+} 
+
+int fib_n1 = fibnum(n-1);
+int fib_n2 = fibnum(n-2); 
+int fib = fib_n1+fib_n2;
+
+return fib;
+
+
+}
+
     
 public static void main(String[] args) {
  System.out.println(fact(5));
  System.out.println(sum(5));
  printnum(5);
+ System.out.println();
+ System.out.println(fibnum(10));
 }
 }
