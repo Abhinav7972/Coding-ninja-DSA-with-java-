46b3bde342f3f9adcbd004f541492008301d11d1
abhinav soni <abhinavsoni316@gmail.com>
2026-08-05 11:57:22 +0530

    impliment method to find sum of n number using recursion

--- java_basics/recursion/recursion_1java.java
+++ java_basics/recursion/recursion_1java.java
@@ -11,11 +11,25 @@ public static int fact(int n)
     int output  = n * smallop; 
 
     return output;
-}
+}   
 
+public static int sum(int n)
+{
+    if(n==0)
+    {
+        return 0;
+    }
+
+    int smallop = sum(n-1);
+
+    int output = n + smallop;
+
+    return output;
+}
 
     
 public static void main(String[] args) {
  System.out.println(fact(5));
+ System.out.println(sum(5));
 }
 }
