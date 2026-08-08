3c96352f5e3749d766730a62c7f2532a1b63adc3
abhinav soni <abhinavsoni316@gmail.com>
2026-08-08 14:35:48 +0530

    impliment program to replace pi with 3.14 from string using recursion

--- java_basics/recursion/recursion_2.java
+++ java_basics/recursion/recursion_2.java
@@ -18,10 +18,34 @@ public static String replace(String s,char a,char b)
     return s.charAt(0) + smallOp;
  }
 
+} 
+
+
+public static String replacePI(String str)
+{
+  if(str.length()<=1)
+  {
+    return str;
+  }
+
+  if(str.charAt(0)=='p' && str.charAt(1)=='i')
+  {
+    String smallSTR = replacePI(str.substring(2));
+
+    return "3.14" + smallSTR;
+  } 
+
+  else 
+  {
+    String smallSTR = replacePI(str.substring(1));
+    
+    return str.charAt(0) + smallSTR;
+  }
 }
 
 
 public static void main(String[] args) {
 System.out.println(replace("axbycx", 'x', 'y'));
+System.out.println(replacePI("apibpi"));
 }
 }
