e8d1e9c58b3190735522996f5fe3c836f91b37d5
abhinav soni <abhinavsoni316@gmail.com>
2026-08-29 16:26:51 +0530

    added reff variable of abstract class

--- java_basics/OOP_3/abstract_keyword/Vehicle_use.java
+++ java_basics/OOP_3/abstract_keyword/Vehicle_use.java
@@ -2,10 +2,12 @@
 
 public class Vehicle_use {
     public static void main(String[] args) {
+        Vehicle v;
         Company c1 = new Company("BMW");
         String company1 = c1.getCompany();
-
+        v = new Company("Audi");
         System.out.println(company1);
         System.out.println(c1.isMotorized());
+        v.print();
     }
 }
