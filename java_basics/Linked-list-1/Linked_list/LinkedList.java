49d1fe13db2834db5b37f498b342202d2d72f70c
abhinav soni <abhinavsoni316@gmail.com>
2026-09-06 14:59:43 +0530

    optimised linked list takinput method

--- java_basics/Linked-list-1/Linked_list/LinkedList.java
+++ java_basics/Linked-list-1/Linked_list/LinkedList.java
@@ -30,19 +30,21 @@ public  static   Node<Integer>Takeinput()
 Scanner sc = new Scanner(System.in);
 int data = sc.nextInt();
 
-Node<Integer>head = null;
+Node<Integer>head = null,tail= null;
 while (data!=-1) {
 Node<Integer> currNode = new Node<Integer>(data);
 if (head==null) {
     head = currNode;
+    tail = currNode;
 }
 else
 {
-Node<Integer>tail = head;
-while (tail.next!=null) {
-    tail=tail.next;
-}
+//Node<Integer>tail = head;
+// while (tail.next!=null) {
+//     tail=tail.next;
+// }
 tail.next = currNode;
+tail = currNode;
 }
 
 data = sc.nextInt();
